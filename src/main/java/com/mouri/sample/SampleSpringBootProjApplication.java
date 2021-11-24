a0318e5a43b6450dc3011fc112cdab46cc7e0e8d
siva krishna <sivakrishna850@gmail.com>
2021-11-24 18:37:25 +0530

    first commit

---
+++ src/main/java/com/mouri/sample/SampleSpringBootProjApplication.java
@@ -0,0 +1,13 @@
+package com.mouri.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouri/sample/SampleSpringBootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouri.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
